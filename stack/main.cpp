ec17dece0dc450b307b4cafcf93c6c0fae2fcdf1
insikdev <insik.dev@gmail.com>
2023-09-07 00:15:56 +0900

    stack

--- stack/main.cpp
+++ stack/main.cpp
@@ -2,5 +2,34 @@
 
 int main(void)
 {
+    {
+        Stack<int> s;
+
+        s.Push(0);
+        s.Push(1);
+        s.Push(2);
+        s.Push(3);
+        s.Push(4);
+        s.Pop();
+        s.Print();
+    }
+
+    { // reverse string
+        Stack<char> s;
+
+        s.Push('H');
+        s.Push('e');
+        s.Push('l');
+        s.Push('l');
+        s.Push('o');
+
+        Stack<char> s2 = s;
+
+        while (!s2.IsEmpty()) {
+            std::cout << s2.Top();
+            s2.Pop();
+        }
+    }
+
     return 0;
 }
--- stack/stack.h
+++ stack/stack.h
@@ -1,4 +1,86 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include "cassert"
+#include "cstring"
+#include "iostream"
+
+template <typename T>
+class Stack {
+
+public:
+    Stack(int capacity = 5)
+        : m_capacity { capacity }
+        , m_data { new T[capacity] } {};
+
+    Stack(const Stack& other)
+        : m_capacity { other.m_capacity }
+        , m_top { other.m_top }
+    {
+        m_data = new T[m_capacity];
+        memcpy(m_data, other.m_data, sizeof(T) * (m_top + 1));
+    }
+
+    ~Stack()
+    {
+        if (m_data) {
+            delete[] m_data;
+        }
+    }
+
+    void Pop()
+    {
+        assert(!IsEmpty());
+
+        m_top--;
+    };
+
+    void Push(const T& data)
+    {
+        if (m_capacity - 1 == m_top) {
+            T* expand = new T[m_capacity * 2];
+            memcpy(expand, m_data, sizeof(T) * Size());
+            delete[] m_data;
+
+            m_data = expand;
+            m_capacity *= 2;
+        }
+
+        m_data[++m_top] = data;
+    };
+
+    T& Top()
+    {
+        assert(!IsEmpty());
+
+        return m_data[m_top];
+    }
+
+    bool IsEmpty()
+    {
+        return m_top == -1;
+    };
+
+    int Size()
+    {
+        return m_top + 1;
+    }
+
+    void Print()
+    {
+        std::cout << "Print Stack" << std::endl;
+
+        for (int i = 0; i <= m_top; i++) {
+            std::cout << m_data[i] << ' ';
+        }
+
+        std::cout << std::endl;
+    }
+
+private:
+    T* m_data = nullptr;
+    int m_top = -1;
+    int m_capacity = 0;
+};
+
 #endif /* STACK_H*/
